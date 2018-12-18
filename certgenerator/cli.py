10a5eef70044e36289255c41318f5b4aebf06854
eviljohndoe <jonathan.ullindah@gmail.com>
2018-12-18 19:51:27 +0100

    Delete options 'debug' and 'verbose' for main command Add options 'debug' and 'verbose' for commands: 'create', 'create-multiple', 'create-p12', 'create_multiple_p12' Improve command 'delete' using context manager

--- certgenerator/cli.py
+++ certgenerator/cli.py
@@ -9,27 +9,30 @@ tools = Tools()
 
 
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('-v', '--verbose', is_flag=True, help="Display only if necessary")
-@click.option('-d', '--debug', is_flag=True, help="Display all details")
 @click.option('-V', '--version', is_flag=True, callback=callbacks.print_version,
               expose_value=False, is_eager=True, help="show version and exit")
-def main(verbose, debug):
+def main():
     """
     A command line tool to create and read CSR and P12
     """
     if tools.get_config(section="custom"):
         tools.set_options(custom=tools.get_config(section="custom"))
 
-    tools.set_options(verbose=verbose, debug=debug)
 
-
-@main.command()
+@main.command(short_help="init app")
 @decorators.folder_options
 @decorators.csv_options
 def init(cert_folder, csv_file, yaml):
     """
+    \b
     Create or edit certificate folder and csv file
     Add yaml file if not exists (csr.yaml)
+    \f
+
+    :param cert_folder:
+    :param csv_file:
+    :param yaml:
+    :return:
     """
     edit_config(cert_folder, csv_file, yaml)
 
@@ -39,11 +42,25 @@ def init(cert_folder, csv_file, yaml):
 @decorators.pass_logger
 @click.argument('name', type=str, required=False)
 @decorators.global_options("csr")
-def create(logger, ctx, name, config, force, key_size, san, **subject):
+@decorators.debug_options
+def create(logger, ctx, name, config, force, key_size, san, verbose, debug, **subject):
     """
     Create a single CSR
+    \f
+
+    :param logger:
+    :param ctx:
+    :param name:
+    :param config:
+    :param force:
+    :param key_size:
+    :param san:
+    :param verbose:
+    :param debug:
+    :param subject:
+    :return:
     """
-    tools.set_options(ctx=ctx, config=config, san=san, size=key_size, subject=subject)
+    tools.set_options(ctx=ctx, config=config, san=san, size=key_size, subject=subject, verbose=verbose, debug=debug)
 
     if name:
         tools.set_options(name=str(name))
@@ -56,16 +73,30 @@ def create(logger, ctx, name, config, force, key_size, san, **subject):
     cert.generate_csr(force=force)
 
 
-@main.command()
+@main.command(short_help="Create multiple CSR")
 @click.pass_context
 @decorators.pass_logger
 @decorators.csv_options
 @decorators.global_options("csr")
-def create_multiple(logger, ctx, csv_file, config, force, key_size, san, **subject):
+@decorators.debug_options
+def create_multiple(logger, ctx, csv_file, config, force, key_size, san, verbose, debug, **subject):
     """
     Create multiple certificate using csv file
+    \f
+
+    :param logger:
+    :param ctx:
+    :param csv_file:
+    :param config:
+    :param force:
+    :param key_size:
+    :param san:
+    :param verbose:
+    :param debug:
+    :param subject:
+    :return:
     """
-    tools.set_options(ctx=ctx, config=config, san=san, size=key_size, subject=subject)
+    tools.set_options(ctx=ctx, config=config, san=san, size=key_size, subject=subject, verbose=verbose, debug=debug)
     cert = Certificate(logger=logger, opts=tools.opts)
 
     if 'subject' in tools.opts:
@@ -77,7 +108,7 @@ def create_multiple(logger, ctx, csv_file, config, force, key_size, san, **subje
         cert.generate_multiple(force=force)
 
 
-@main.command()
+@main.command(short_help="Create one p12")
 @click.pass_context
 @decorators.pass_logger
 @click.argument('name', type=str)
@@ -85,12 +116,27 @@ def create_multiple(logger, ctx, csv_file, config, force, key_size, san, **subje
 @click.option('-k', '--key', type=str)
 @click.option('-pass', '--password', type=str, hide_input=True, help="Define password, default is '3z6F2Xfc'")
 @decorators.global_options("p12")
-def create_p12(logger, ctx, name, pem, key, password, config, force):
+@decorators.debug_options
+def create_p12(logger, ctx, name, pem, key, password, config, force, verbose, debug):
     """
+    \b
     Create a simple p12
     Need key file and pem file
+    \f
+
+    :param logger:
+    :param ctx:
+    :param name:
+    :param pem:
+    :param key:
+    :param password:
+    :param config:
+    :param force:
+    :param verbose:
+    :param debug:
+    :return:
     """
-    tools.set_options(ctx=ctx, config=config)
+    tools.set_options(ctx=ctx, config=config, verbose=verbose, debug=debug)
 
     cert = Certificate(logger=logger, opts=tools.opts)
     if password:
@@ -99,7 +145,7 @@ def create_p12(logger, ctx, name, pem, key, password, config, force):
         cert.generate_p12(key=key, pem=pem, p12=name, force=force)
 
 
-@main.command()
+@main.command(short_help="Create multiple p12")
 @click.pass_context
 @decorators.pass_logger
 @decorators.csv_options
@@ -108,11 +154,24 @@ def create_p12(logger, ctx, name, pem, key, password, config, force):
               help="Define key folder where all the key are located,"
                    " if not defined, it will search key in certificate folder")
 @decorators.global_options("p12")
-def create_multiple_p12(logger, ctx, csv_file, pem_folder, key_folder, config, force):
+@decorators.debug_options
+def create_multiple_p12(logger, ctx, csv_file, pem_folder, key_folder, config, force, verbose, debug):
     """
     Create multiple p12 using csv file
+    \f
+
+    :param logger:
+    :param ctx:
+    :param csv_file:
+    :param pem_folder:
+    :param key_folder:
+    :param config:
+    :param force:
+    :param verbose:
+    :param debug:
+    :return:
     """
-    tools.set_options(ctx=ctx, config=config)
+    tools.set_options(ctx=ctx, config=config, verbose=verbose, debug=debug)
 
     cert = Certificate(logger, opts=tools.opts)
     if csv_file:
@@ -128,7 +187,17 @@ def create_multiple_p12(logger, ctx, csv_file, pem_folder, key_folder, config, f
 @click.option('-pass', '--password', type=str, hide_input=True, help="password used for create p12")
 @click.option('-t', '--plain-text', is_flag=True, help="Display certificate in plain text instead of json")
 def read(logger, ctx, path, password, plain_text):
-    """Read csr or p12"""
+    """
+    Read csr or p12
+    \f
+
+    :param logger:
+    :param ctx:
+    :param path:
+    :param password:
+    :param plain_text:
+    :return:
+    """
     tools.set_options(ctx=ctx)
 
     cert = Certificate(logger=logger, opts=tools.opts)
@@ -151,8 +220,7 @@ def config():
 
 @config.command()
 def read():
-    """
-    read config ini
+    """read config ini
     """
     app_folder = None
     if tools.app_folder_exists():
@@ -166,13 +234,20 @@ def read():
     click.echo("+++++csr.yaml+++++\n{y}".format(y=list_yaml))
 
 
-@config.command()
+@config.command(short_help="edit app config")
 @decorators.folder_options
 @decorators.csv_options
 def edit(cert_folder, csv_file, yaml):
     """
+    \b
     Create or edit certificate folder and csv file
     Add yaml file if not exists (csr.yaml)
+    \f
+
+    :param cert_folder:
+    :param csv_file:
+    :param yaml:
+    :return:
     """
     edit_config(cert_folder, csv_file, yaml)
 
@@ -185,29 +260,30 @@ def edit_yaml():
     tools.write_yaml()
 
 
-@config.command()
+@config.command(short_help="Delete options")
 @click.option("-cert", "--cert-folder", is_flag=True, help="if flag, delete cert path folder from config ini")
 @click.option("-csv", "--csv-file", is_flag=True, help="if flag, delete csvfile from config ini")
 def delete(cert_folder, csv_file):
     """
     Delete option from config ini (csr, p12 and csv path)
+    \f
+
+    :param cert_folder:
+    :param csv_file:
+    :return:
     """
     _config = tools.get_config()
 
-    def check_section():
-        if not _config.get_section(section="custom"):
-            _config.remove_section(section="custom")
-
-    if cert_folder:
-        _config.remove_option(section="custom", option="cert_directory")
-        check_section()
-
-    if csv_file:
-        _config.remove_option(section="custom", option="csvfile")
-        check_section()
-
     if not cert_folder and not csv_file:
         _config.remove_section(section="custom")
+
+    with decorators.RemoveOption(config=_config, option=cert_folder) as o:
+        if o:
+            _config.remove_option(section="custom", option="app_folder")
+
+    with decorators.RemoveOption(config=_config, option=csv_file) as o:
+        if o:
+            _config.remove_option(section="custom", option="csvfile")
 
 
 if __name__ == "__main__":
